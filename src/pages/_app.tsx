e0e894e49adca75a2db92af900d06c5db71277f8
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-10-12 20:16:48 +0900

    Localized amplify auth.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
-import * as React from 'react';
 import { AppProps } from 'next/app';
 import PropTypes from 'prop-types';
-import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from 'styles/theme';
 import createEmotionCache from 'utilities/create-emotion-cache';
+import { I18n } from 'aws-amplify';
+import { L10n } from 'utilities/l10n';
+import 'styles/globals.css';
+
+I18n.setLanguage('ja'); // Add
+I18n.putVocabularies(L10n); // Add
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
