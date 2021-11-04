69e76cfe2ad5cb89e7e82418cfafc5df54cbc352
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2021-11-04 20:11:15 +0900

    Refoctoring subscription order list

--- src/components/organisms/subscription-orders/subscription-order-list.tsx
+++ src/components/organisms/subscription-orders/subscription-order-list.tsx
@@ -1,18 +1,19 @@
-import { useFetchSubscriptionOrderList } from 'hooks/subscription-orders/use-fetch-subscription-order-list';
-import { ErrorAlert } from 'components/atoms/error-alert';
-import { UpdateSubscriptionOrderButton } from './update-subscription-order-button';
-import { DeleteSubscriptionOrderButton } from './delete-subscription-order-button';
-import { styled } from '@mui/material/styles';
+import { CircularProgress, Typography } from '@mui/material';
+import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
-import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { CircularProgress, Typography } from '@mui/material';
+import { ErrorAlert } from 'components/atoms/error-alert';
+import { EmptyTableBody } from 'components/atoms/tables/empty-table-body';
+import { StyledTableCell } from 'components/atoms/tables/styled-table-cell';
 import { useFormatDate } from 'hooks/date-hooks/use-format-date';
 import { useFormatDateHourMinute } from 'hooks/date-hooks/use-format-date-hour-minute';
+import { useFetchSubscriptionOrderList } from 'hooks/subscription-orders/use-fetch-subscription-order-list';
+import { DeleteSubscriptionOrderButton } from './delete-subscription-order-button';
+import { UpdateSubscriptionOrderButton } from './update-subscription-order-button';
+import { StyledTableRow } from 'components/atoms/tables/styled-table-row';
 
 const header = [
   {
@@ -37,36 +38,6 @@ const header = [
   },
 ];
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.primary.light,
-    color: theme.palette.text.secondary,
-  },
-  // [`&.${tableCellClasses.body}`]: {
-  //   fontSize: 14,
-  // },
-}));
-
-const EmptyTableBody: React.FC = ({ children }) => {
-  return (
-    <StyledTableRow>
-      <StyledTableCell colSpan={header.length} align='center'>
-        {children}
-      </StyledTableCell>
-    </StyledTableRow>
-  );
-};
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  '&:nth-of-type(even)': {
-    backgroundColor: theme.palette.grey[50],
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0,
-  },
-}));
-
 export const SubscriptionOrderList = () => {
   const { error, data } = useFetchSubscriptionOrderList();
   const { formatDate } = useFormatDate();
@@ -88,13 +59,12 @@ export const SubscriptionOrderList = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {!data ? (
-            <EmptyTableBody>
-              <CircularProgress />
+          {(!data || data.length == 0) && (
+            <EmptyTableBody headerLength={header.length}>
+              {!data ? <CircularProgress /> : '在定期便はありません'}
             </EmptyTableBody>
-          ) : data.length == 0 ? (
-            <EmptyTableBody>定期便を追加してください</EmptyTableBody>
-          ) : (
+          )}
+          {data &&
             data.map((item) => (
               <StyledTableRow key={item.id}>
                 <StyledTableCell>{item.staff.name}</StyledTableCell>
@@ -107,8 +77,7 @@ export const SubscriptionOrderList = () => {
                   <DeleteSubscriptionOrderButton id={item.id} />
                 </StyledTableCell>
               </StyledTableRow>
-            ))
-          )}
+            ))}
         </TableBody>
       </Table>
     </TableContainer>
