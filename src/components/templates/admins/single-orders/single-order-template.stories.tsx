03da743f03dfc152b2ab387e5c07986b16984087
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-03-11 18:56:57 +0900

    Added dummy stories description.

--- src/components/templates/admins/single-orders/single-order-template.stories.tsx
+++ src/components/templates/admins/single-orders/single-order-template.stories.tsx
@@ -2,12 +2,35 @@ import type { ComponentStoryObj } from '@storybook/react';
 import { adminOrderListMock, orderListMock } from 'mocks/order-list.mock';
 import { SingleOrderTemplate } from './single-order-template';
 
+const description = `
+
+## Use Case
+
+description
+
+	dummy
+	dummy
+
+## Specs
+
+## Back Office Ops
+
+`;
+
 type Story = ComponentStoryObj<typeof SingleOrderTemplate>;
 
 export default { component: SingleOrderTemplate };
 
 export const Default: Story = {
   args: { data: adminOrderListMock },
+};
+
+Default.parameters = {
+  docs: {
+    description: {
+      component: description,
+    },
+  },
 };
 
 export const Loading: Story = {
