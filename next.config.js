2f7d40fcbef27f7d4f22fcdc7d74b5fa6cc92396
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-05-12 19:55:45 +0900

    The reactStrictMode changed true.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  // reactStrictMode: true,
-  reactStrictMode: false, // trueの場合react-beautiful-dndを使用しているpageでerrorが発生する為falseに変更。debug buildでStrictModeで検証が必要な場合は必ずtrueに変更すること
+  reactStrictMode: true,
+  // reactStrictMode: false, // trueの場合react-beautiful-dndを使用しているpageでerrorが発生する為falseに変更。debug buildでStrictModeで検証が必要な場合は必ずtrueに変更すること
   swcMinify: true, // use SWC for minification
 };
