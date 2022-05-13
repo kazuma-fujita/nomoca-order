e8fd80ddad3f161bc743ac677b8e85f315586bd0
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-05-13 14:43:59 +0900

    added amplify.yml and executed amplify env add prod

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,23 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "nomocaorder0f71faa3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        },
+        "userPoolGroups": {
+            "AdminsGroupRole": "string",
+            "OperatorsGroupRole": "string"
+        }
+    },
+    "api": {
+        "nomocaorderapi": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
