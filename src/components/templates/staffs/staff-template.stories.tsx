b1ffc7a1406bf31f22316179a23ba81a89685437
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-05-25 20:32:55 +0900

    Added chromatic modules

--- src/components/templates/staffs/staff-template.stories.tsx
+++ src/components/templates/staffs/staff-template.stories.tsx
@@ -3,6 +3,11 @@ import { staffListMock } from 'mocks/staff.mock';
 import { graphql } from 'msw';
 import { StaffListContextProvider } from 'hooks/staffs/use-fetch-staff-list';
 import { StaffTemplate } from './staff-template';
+import awsconfig from 'aws-exports';
+import { Amplify } from 'aws-amplify';
+
+// Cognito認証でAppSyncを実行するとNo current user errorが発生する為、API_KEY認証に切り替え
+Amplify.configure({ ...awsconfig, aws_appsync_authenticationType: 'API_KEY' });
 
 type Story = ComponentStoryObj<typeof StaffTemplate>;
 
