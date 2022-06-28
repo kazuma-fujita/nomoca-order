e2cee83e67929e3df2451c4420a0fc78898870d4
kazuma-fujita <kazuma.fujita.zuma@gmail.com>
2022-06-28 17:35:37 +0900

    added amplify_init.sh

---
+++  amplify_init.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+IFS=’|’
+
+REACTCONFIG=”{\
+\”SourceDir\”:\”src\”,\
+\”DistributionDir\”:\”out\”,\
+\”BuildCommand\”:\”npm run-script build\”,\
+\”StartCommand\”:\”npm run-script start\”\
+}”
+
+AWSCLOUDFORMATIONCONFIG=”{\
+\”configLevel\”:\”project\”,\
+\”useProfile\”:true,\
+\”profileName\”:\”default\”,\
+\"accessKeyId\":\"$AWS_ACCESS_KEY_ID\",\
+\"secretAccessKey\":\"$AWS_SECRET_ACCESS_KEY\",\
+\"region\":\"$AWS_REGION\"\
+}”
+
+AMPLIFY=”{\
+\”projectName\”:\”nomoca-order\”,\
+\”envName\”:\”devlop\”,\
+\”defaultEditor\”:\”code\”\
+}”
+
+FRONTEND=”{\
+\”frontend\”:\”javascript\”,\
+\”framework\”:\”react\”,\
+\”config\”:$REACTCONFIG\
+}”
+
+PROVIDERS=”{\
+\”awscloudformation\”:$AWSCLOUDFORMATIONCONFIG\
+}”
+amplify init \
+—- amplify $AMPLIFY \
+-- frontend $FRONTEND \
+-- providers $PROVIDERS \
+-- yes
